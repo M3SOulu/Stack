2d1f383c315892d949ed2dbfff6d43f110da5b3f
mauri <mauri@172.26.194.157>
2016-11-11 10:14:28 +0100

    Manca l'implementazione del metodo peek e relativi test

---
+++ src/it/uniba/tutorial/tests/NegativeNumberException.java
@@ -0,0 +1,10 @@
+package it.uniba.tutorial.tests;
+
+public class NegativeNumberException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
--- src/it/uniba/tutorial/tests/StackTest.java
+++ src/it/uniba/tutorial/tests/StackTest.java
@@ -6,19 +6,68 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import it.uniba.tutorial.Stack;
+import it.uniba.tutorial.cannotPopFromEmptyStackException;
+
 public class StackTest {
+	Stack myStack;
 
 	@Before
 	public void setUp() throws Exception {
+		//Arrange
+		myStack = new Stack();
 	}
 
 	@After
 	public void tearDown() throws Exception {
 	}
-
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void addingTheFirstElementHasSizeOne() throws NegativeNumberException{
+		//Act
+		myStack.push(3);
+		//Assert
+		assertEquals(1, myStack.size());
 	}
+	
+	@Test(expected = NegativeNumberException.class)
+	public void addNegativeNumberShouldRaiseAnException () throws NegativeNumberException{
+		//Act
+		myStack.push(-1000);
+	}
+	
+	@Test
+	public void  stackIsNotEmptyAfterAddingOneElement() throws NegativeNumberException{
+		//Act
+		myStack.push(45);
+		//Assert
+		assertFalse(myStack.isEmpty());
+	}
+	
+	@Test
+	public void aJustCreatedStackIsEmpty(){
+		assertTrue(myStack.isEmpty());
+	}
+	
+	@Test
+	public void popRemovesLastElement() throws NegativeNumberException, cannotPopFromEmptyStackException{
+		//Arrange
+		myStack.push(1);
+		myStack.push(10);
+		myStack.push(100);
+		
+		//Act
+		myStack.pop();
+		
+		//Assert
+		assertEquals(2, myStack.size());
+	}
+	
+	@Test(expected = cannotPopFromEmptyStackException.class)
+	public void popFromEmptyStackShouldRaiseAnException() throws cannotPopFromEmptyStackException, NegativeNumberException{
+		//Act
+		myStack.pop();
+	}
+	
 
 }
