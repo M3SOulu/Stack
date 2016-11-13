2894815be5548730f6801baebac197e0c64ceae1
michelecafagna26 <Michele@Michele_PC.homenet.telecomitalia.it>
2016-11-13 16:52:16 +0100

    implemented stack methods add some tests

--- src/it/uniba/tutorial/Stack.java
+++ src/it/uniba/tutorial/Stack.java
@@ -1,12 +1,80 @@
 package it.uniba.tutorial;
 
-public class Stack {
+import java.util.LinkedList;
+
+/**
+ * Simulate a stack structure to perform storing data in LIFO  
+ * @author Michele
+ *
+ */
+public class Stack{
 	
-	public void push(int x) {}
-	public void pop(){}
-	public int peek(){ return -1;}
-	public int size(){ return -1;}
-	public boolean isEmpty() {return false;}
+	private LinkedList<Integer> stack;
 	
+	/**
+	 * constructor
+	 */
+	public Stack(){
+		
+		stack=new LinkedList<Integer>();
+	}
+	
+	/**
+	 * push an element to the top of the stack
+	 * @param x
+	 */
+	public void push(int x) { stack.addFirst(x); }
+	
+	/**
+	 * pop the last item pushed in the stack
+	 */
+	public void pop() throws EmptyStackException{ 
+		
+		if(!isEmpty()){
+			
+			stack.removeFirst(); 
+			
+		}else{
+			
+			throw new EmptyStackException("illegal action!");
+		}
+		
+	}
+	
+	/**
+	 * return the top of the satck
+	 * @return the first element of the stack, min integer value otherwise.
+	 */
+	public int peek() throws EmptyStackException{ 
+		
+		if(isEmpty()){
+			
+			throw new EmptyStackException();
+			
+		}
+		
+		return stack.getFirst();
+		
+	}
+	
+	/**
+	 * 
+	 * @return the size of the stack
+	 */
+	public int size(){ return stack.size(); }
+	
+	/**
+	 *  
+	 * @return true if the stack is empty, false otherwise.
+	 */
+	public boolean isEmpty() { return stack.isEmpty(); }
+	
+	/**
+	 * remove all items from the stack
+	 */
+	public void rifle(){
+		
+		stack.removeAll(stack);
+	}
 
 }
--- src/it/uniba/tutorial/tests/StackTest.java
+++ src/it/uniba/tutorial/tests/StackTest.java
@@ -6,19 +6,48 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class StackTest {
+import it.uniba.tutorial.EmptyStackException;
+import it.uniba.tutorial.Stack;
+import junit.framework.Assert;
 
+/**
+* tests for Stack class
+*/
+public class StackTest {
+	
+	private Stack stack;
+	
 	@Before
 	public void setUp() throws Exception {
+		
+		stack= new Stack();
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		
+		stack.rifle();
 	}
-
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testIsEmpty(){
+		
+		Assert.assertTrue(stack.isEmpty());
+	}
+	@Test
+	public void testPushAndPeek() throws EmptyStackException{
+		
+		stack.push(1);
+		Assert.assertEquals(stack.peek(),1);
+	}
+	
+	
+	@Test
+	public void testPop()  throws EmptyStackException{
+		
+		stack.push(1);
+		stack.pop();
+		Assert.assertTrue(stack.isEmpty());
 	}
 
 }
