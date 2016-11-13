85fa89eb1f8445ae502d1a32057e09ad3f0a94fe
andre <andre@DESKTOP-NIHUKT8>
2016-11-13 21:15:21 +0100

    Completata esercitazione Stack

---
+++ src/it/uniba/tutorial/NegativeNumberException.java
@@ -0,0 +1,5 @@
+package it.uniba.tutorial;
+
+public class NegativeNumberException extends Exception {
+
+}
--- src/it/uniba/tutorial/tests/StackTest.java
+++ src/it/uniba/tutorial/tests/StackTest.java
@@ -6,10 +6,16 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import it.uniba.tutorial.IllegalRemoveException;
+import it.uniba.tutorial.NegativeNumberException;
+import it.uniba.tutorial.Stack;
+
 public class StackTest {
+	Stack myStack;
 
 	@Before
 	public void setUp() throws Exception {
+		myStack = new Stack();
 	}
 
 	@After
@@ -17,8 +23,66 @@ public class StackTest {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void addingTheFirstElementHasSizeOne() throws NegativeNumberException {
+
+		// Arrange
+
+		// Act
+		myStack.push(3);
+		// Assert
+		assertEquals(1, myStack.size());
 	}
 
+	@Test(expected = NegativeNumberException.class)
+	public void addNegativeNumberShouldRaiseAnException() throws NegativeNumberException {
+		// Arrange
+		// Act
+		myStack.push(-1000);
+	}
+
+	@Test
+	public void stackIsNotEmptyAfterAddingOneElement() throws NegativeNumberException {
+		// Act
+		myStack.push(42);
+
+		// Assert
+		assertFalse(myStack.isEmpty());
+
+	}
+
+	@Test
+	public void aJustCreatedStackIsEmpty() {
+		assertTrue(myStack.isEmpty());
+	}
+
+	@Test
+	public void popRemovesLastElement() throws NegativeNumberException, IllegalRemoveException {
+		// Arrange
+		myStack.push(1);
+		myStack.push(10);
+		myStack.push(100);
+
+		// Act
+		myStack.pop();
+
+		// Assert
+		assertEquals(2, myStack.size());
+	}
+
+	@Test(expected = IllegalRemoveException.class)
+	public void cannotPopFromAnEmptyStack() throws IllegalRemoveException {
+		// Act
+		myStack.pop();
+	}
+
+	@Test
+	public void peekGetLastElement() throws NegativeNumberException {
+		// Arrange
+		myStack.push(1);
+		myStack.push(10);
+		myStack.push(100);
+
+		// Assert
+		assertEquals(100, myStack.peek());
+	}
 }
